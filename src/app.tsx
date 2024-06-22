ecbf4160688d552f162cdbac4a8ab0c39f807ff1
cgadd21 <ctg7866@g.rit.edu>
2024-06-22 14:59:29 -0400

    init

---
+++ src/app.tsx
@@ -0,0 +1,40 @@
+export const App = () => {
+  return (
+    <article className="prose">
+      <h1>Cooper Gadd</h1>
+      <div>
+        <h2>Socials</h2>
+        <p>
+          <a href="https://github.com/cooper-gadd">GitHub: cooper-gadd</a>
+        </p>
+        <p>
+          <a href="https://www.linkedin.com/in/coopergadd/">
+            LinkedIn: Cooper Gadd
+          </a>
+        </p>
+        <p>
+          <a href="mailto:coopergadd@yahoo.com">Email: coopergadd@yahoo.com</a>
+        </p>
+        <p>
+          <a href="https://www.ctgadd.com">Website: ctgadd.com</a>
+        </p>
+      </div>
+      <div>
+        <h2>Objective</h2>
+        <p>
+          Seeking a software engineering position where I can leverage my
+          experience in full-stack development to create innovative solutions to
+          complex problems.
+        </p>
+      </div>
+      <div>
+        <h2>Education</h2>
+        <p>
+          Rochester Institute of Technology, B.S. in Computing and Information
+          Technologies
+        </p>
+        <p>GPA: 3.8</p>
+      </div>
+    </article>
+  );
+};
